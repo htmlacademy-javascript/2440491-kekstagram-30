7b4a2c2bba78a8528ea490fa5bc4b9648d98c21d
MicroDiamond5004 <serepetr90@mail.ru>
2023-10-20 22:24:11 +0300

    task 1 module 5

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import { getPhotoDescription } from "./create-descriptions.js";
+import { getPhotoDescription } from './create-descriptions.js';
 
 // генерируем 25 описаний для фото
 const photoDescriptions = Array.from({length: 25}, getPhotoDescription);
