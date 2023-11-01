ffd40274add76db26ee10c9faa0435fbf60fb401
MicroDiamond5004 <serepetr90@mail.ru>
2023-11-01 11:45:45 +0300

    module 7 task 1 complete

--- js/main.js
+++ js/main.js
@@ -1,5 +1,2 @@
-import { getPhotoDescription } from './create-descriptions.js';
-
-// генерируем 25 описаний для фото
-// eslint-disable-next-line no-unused-vars
-const photoDescriptions = Array.from({length: 25}, getPhotoDescription);
+import { createPhotos } from './create-photos';
+createPhotos();
