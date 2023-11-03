19ad6cd52de1b19b43a55f09dac0b3debb4ad900
MicroDiamond5004 <serepetr90@mail.ru>
2023-11-03 23:53:46 +0300

    module 8 task 1

--- js/create-photos.js
+++ js/create-photos.js
@@ -1,11 +1,13 @@
+import { openBigPhoto } from './open-fullsize-photo';
 const pictures = document.querySelector('.pictures');
 const template = document.querySelector('#picture').content.querySelector('.picture');
 
 const fragment = document.createDocumentFragment();
 
 // Ф-ция для добавления фото в разметку
-const createPhotos = function (photoDescriptions) {
+const createPhotos = function (getPhotoDescription) {
   // генерируем 25 описаний для фото
+  const photoDescriptions = Array.from({length: 24}, getPhotoDescription);
   photoDescriptions.forEach((element) => {
     const photoCard = template.cloneNode(true);
     photoCard.querySelector('.picture__img').src = element.url;
@@ -16,5 +18,6 @@ const createPhotos = function (photoDescriptions) {
   });
 
   pictures.append(fragment);
+  openBigPhoto(photoDescriptions);
 };
 export {createPhotos};
--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
 import { getPhotoDescription } from './create-descriptions';
 import { createPhotos } from './create-photos';
-import { openBigPhoto } from './open-fullsize-photo';
 
-const photoDescriptions = Array.from({length: 24}, getPhotoDescription);
-createPhotos(photoDescriptions);
-openBigPhoto(photoDescriptions);
+createPhotos(getPhotoDescription);
