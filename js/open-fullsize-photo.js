a5c69f1b0aba26b0cfa304953fd0775cf4c0d49d
MicroDiamond5004 <serepetr90@mail.ru>
2023-11-05 00:54:49 +0300

    module 8 task 2

--- js/open-fullsize-photo.js
+++ js/open-fullsize-photo.js
@@ -1,4 +1,4 @@
-import { createNewElement, getNumInRange } from './util';
+import { createNewElement } from './util.js';
 
 const body = document.querySelector('body');
 const popUp = document.querySelector('.big-picture');
@@ -25,34 +25,57 @@ const countCommentsBlock2 = document.querySelector('.comments-loader');
 
 let countImage = 0;
 
-const closeAll = function () {
-  popUp.classList.add('hidden');
-  countCommentsBlock.classList.remove('hidden');
-  countCommentsBlock2.classList.remove('hidden');
-  body.classList.remove('modal-open');
-};
-
+// Функция для загрузки комментариев
+const loadComments = function (number, maxNum, photoComments) {
+  for (let i = number; i < maxNum; i++){
+    const photoComment = photoComments[i];
+    const commentLi = createNewElement('li','social__comment');
+    const commentImg = createNewElement('img','social__picture');
+    commentImg.src = photoComment.avatar;
+    commentImg.alt = photoComment.name;
+    commentImg.width = 35;
+    commentImg.height = 35;
+    const commentText = createNewElement('p', 'social__text', photoComment.message);
+    commentLi.append(commentImg);
+    commentLi.append(commentText);
+    commentsBlock.append(commentLi);
+  }
+}
 // Функция для создания комментариев к фото
 const createPhotoComments = function (photoComments) {
   commentsBlock.innerHTML = '';
   if (photoComments !== 0){
     let countOfComments = 0;
-    for (let i = 0; i < getNumInRange(1, photoComments.length); i++){
-      const photoComment = photoComments[i];
-      const commentLi = createNewElement('li','social__comment');
-      const commentImg = createNewElement('img','social__picture');
-      commentImg.src = photoComment.avatar;
-      commentImg.alt = photoComment.name;
-      commentImg.width = 35;
-      commentImg.height = 35;
-      const commentText = createNewElement('p', 'social__text', photoComment.message);
-      commentLi.append(commentImg);
-      commentLi.append(commentText);
-      commentsBlock.append(commentLi);
-      countOfComments++;
-    }
-    commentsShown.textContent = countOfComments;
-    commentsAll.textContent = photoComments.length;
+    const maxNumber = photoComments.length;
+    const checkComments = function () {
+      if ((maxNumber - countOfComments) > 5) {
+        loadComments(countOfComments, countOfComments + 5, photoComments);
+        countOfComments += 5;
+        commentsShown.textContent = countOfComments;
+        countCommentsBlock2.classList.remove('hidden');
+      } else {
+        loadComments(countOfComments, maxNumber, photoComments);
+        commentsShown.textContent = photoComments.length;
+        countCommentsBlock2.classList.add('hidden');
+        countCommentsBlock2.removeEventListener('click', checkComments);
+      }
+      commentsAll.textContent = photoComments.length;
+    };
+    countCommentsBlock2.addEventListener('click', checkComments);
+    // Закрыть все!
+    const closeAll = function () {
+      popUp.classList.add('hidden');
+      body.classList.remove('modal-open');
+      countCommentsBlock2.removeEventListener('click', checkComments);
+    };
+    closeButton.addEventListener('click', closeAll);
+    document.addEventListener('keydown', (evt) => {
+      if (evt.key === 'Escape'){
+        closeAll();
+      }
+    });
+    //
+    checkComments();
   }
 };
 
@@ -70,8 +93,6 @@ const openPopUp = function (el, photoDescriptions, photoNumber) {
   el.addEventListener('click', () => {
     changePhotoCard(photoDescriptions[photoNumber]);
     popUp.classList.remove('hidden');
-    countCommentsBlock.classList.add('hidden');
-    countCommentsBlock2.classList.add('hidden');
     body.classList.add('modal-open');
   });
 };
@@ -82,12 +103,6 @@ const openBigPhoto = function (photoDescriptions) {
   miniPictures.forEach((element) => {
     openPopUp(element, photoDescriptions, countImage);
     countImage++;
-  });
-  closeButton.addEventListener('click', closeAll);
-  document.addEventListener('keydown', (evt) => {
-    if (evt.key === 'Escape'){
-      closeAll();
-    }
   });
 };
 
