12239fe5dbf7153fb28d0c7a67fe47f967c1d412
MicroDiamond5004 <serepetr90@mail.ru>
2023-11-24 01:37:52 +0300

    module11-task1

--- js/change-form.js
+++ js/change-form.js
@@ -1,6 +1,7 @@
 import '../vendor/pristine/pristine.min.js';
 import '../vendor/nouislider/nouislider.js';
 import '../vendor/nouislider/nouislider.css';
+import { sendData } from './api.js';
 
 const body = document.querySelector('body');
 const form = body.querySelector('.img-upload__form');
@@ -16,86 +17,23 @@ let validOption = true;
 let validOption2 = true;
 let currentScale = 1;
 
+const sliderEl = mainPopup.querySelector('.effect-level__slider');
+const sliderValue = mainPopup.querySelector('.effect-level__value');
+
+const buttonScaleMinus = mainPopup.querySelector('.scale__control--smaller');
+const buttonScalePlus = mainPopup.querySelector('.scale__control--bigger');
+const scaleInput = mainPopup.querySelector('.scale__control--value');
+
 const pristine = new Pristine(form, {
   classTo: 'img-upload__field-wrapper',
-  errorClass: 'form__item--invalid',
+  errorClass: 'img-upload__field-wrapper--error',
   successClass: 'form__item--valid',
   errorTextParent: 'img-upload__field-wrapper',
   errorTextTag: 'span',
   errorTextClass: 'form__error'
 });
 
-// Загрузить попап с изображением
-const uploadImage = function () {
-  const disableButton = function (button) {
-    button.disabled = true;
-  };
-  const enableButton = function (button) {
-    button.disabled = false;
-  };
-  const toggleButtonFromInput = function (button, input, condition) {
-    if ((input.value === '' || condition) && validOption && validOption2) {
-      enableButton(button);
-    } else {
-      disableButton(button);
-    }
-    return (input.value === '' || condition);
-  };
-  const disabledForClose = function (el) {
-    el.addEventListener('keydown', (evt) => {
-      evt.stopPropagation();
-    });
-  };
-  const addHashtag = function () {
-    const hashtag = /^#[a-zа-яё0-9]{1,19}$/i;
-    const checkHashtag = function () {
-      const validHashtag = hashtag.test(hashtagsInput.value);
-      validOption = toggleButtonFromInput(submitButton, hashtagsInput, validHashtag);
-      toggleButtonFromInput(submitButton, hashtagsInput, validHashtag);
-      return hashtag;
-    };
-    disabledForClose(hashtagsInput);
-    pristine.addValidator(hashtagsInput, checkHashtag);
-  };
-  const addComment = function () {
-    const checkCommentLength = function () {
-      const validComment = commentText.value.length <= 140;
-      validOption2 = toggleButtonFromInput(submitButton, commentText, validComment);
-      toggleButtonFromInput(submitButton, commentText, validComment);
-      return validComment;
-    };
-    disabledForClose(commentText);
-    pristine.addValidator(commentText, checkCommentLength);
-  };
-  inputChangePhoto.onchange = function () {
-    mainPopup.classList.remove('hidden');
-    body.classList.add('modal-open');
-    addHashtag();
-    addComment();
-  };
-
-  // Закрыть всё
-  const closeAll = function () {
-    inputChangePhoto.value = '';
-    mainPopup.classList.add('hidden');
-    body.classList.remove('modal-open');
-    previewPhoto.style.transform = null;
-    currentScale = 1;
-  };
-
-  buttonClose.addEventListener('click', closeAll);
-  window.addEventListener('keydown', (evt) => {
-    if (evt.key === 'Escape'){
-      closeAll();
-    }
-  });
-
-};
-
-const buttonScaleMinus = mainPopup.querySelector('.scale__control--smaller');
-const buttonScalePlus = mainPopup.querySelector('.scale__control--bigger');
-const scaleInput = mainPopup.querySelector('.scale__control--value');
-
+// Ф-ция для изменения масштаба фото
 const changeScale = function () {
   const checkScaleButton = function(operator) {
     let checkScaleValue;
@@ -121,10 +59,11 @@ const changeScale = function () {
   });
 };
 
+const filtersEl = mainPopup.querySelectorAll('.effects__radio');
+
+// Ф-ция для создания фильтра для фото
 const createPhotoFilter = function () {
   let lastFilter;
-  const sliderEl = mainPopup.querySelector('.effect-level__slider');
-  const sliderValue = mainPopup.querySelector('.effect-level__value');
   sliderEl.parentElement.classList.add('hidden');
   // Создаем слайдер
   noUiSlider.create(sliderEl, {
@@ -155,7 +94,6 @@ const createPhotoFilter = function () {
     phobos: '0:3:0.1:px',
     heat: '1:3:0.1',
   };
-  const filtersEl = mainPopup.querySelectorAll('.effects__radio');
 
   // Ф-ция для смены фильтра
   const changeFilter = function (filter) {
@@ -194,6 +132,139 @@ const createPhotoFilter = function () {
     filterEl.addEventListener('click', () => {
       changeFilter(filterEl.value);
     });
+  });
+};
+
+const errorMesages = {
+  Hashtag: 'Хэштэг должен включать в себя только: буквы из русского или английского алфавитов, любые цифры и начинаться с #',
+  Comment: 'Число символов превышает 140, сократите ваш текст пожалуйста'
+};
+
+// Загрузить попап с изображением
+const uploadImage = function () {
+  const disableButton = function (button) {
+    button.disabled = true;
+  };
+  const enableButton = function (button) {
+    button.disabled = false;
+  };
+  const toggleButtonFromInput = function (button, input, condition) {
+    if ((input.value === '' || condition) && validOption && validOption2) {
+      enableButton(button);
+    } else {
+      disableButton(button);
+    }
+    return (input.value === '' || condition);
+  };
+  const disabledForClose = function (el) {
+    el.addEventListener('keydown', (evt) => {
+      evt.stopPropagation();
+    });
+  };
+  const addHashtag = function () {
+    const hashtag = /^#[a-zа-яё0-9]{1,19}$/i;
+    const checkHashtag = function () {
+      const value = hashtagsInput.value;
+      let validHashtag = true;
+      if (value !== ''){
+        validHashtag = hashtag.test(value);
+        validOption = toggleButtonFromInput(submitButton, hashtagsInput, validHashtag);
+        toggleButtonFromInput(submitButton, hashtagsInput, validHashtag);
+      }
+      return validHashtag;
+    };
+    disabledForClose(hashtagsInput);
+    pristine.addValidator(hashtagsInput, checkHashtag, errorMesages.Hashtag);
+  };
+  const addComment = function () {
+    const checkCommentLength = function () {
+      const validComment = commentText.value.length <= 140;
+      validOption2 = toggleButtonFromInput(submitButton, commentText, validComment);
+      toggleButtonFromInput(submitButton, commentText, validComment);
+      return validComment;
+    };
+    disabledForClose(commentText);
+    pristine.addValidator(commentText, checkCommentLength, errorMesages.Comment);
+  };
+  inputChangePhoto.onchange = function () {
+    mainPopup.classList.remove('hidden');
+    body.classList.add('modal-open');
+    addHashtag();
+    addComment();
+  };
+
+  // Закрыть всё
+  const closeAll = function () {
+    inputChangePhoto.value = '';
+    hashtagsInput.value = '';
+    commentText.value = '';
+    mainPopup.classList.add('hidden');
+    body.classList.remove('modal-open');
+    previewPhoto.style.transform = null;
+    currentScale = 1;
+    scaleInput.value = '100%';
+    submitButton.textContent = 'Опубликовать';
+    sliderEl.parentElement.classList.add('hidden');
+    previewPhoto.style.filter = null;
+    filtersEl[0].click();
+  };
+
+  buttonClose.addEventListener('click', closeAll);
+  window.addEventListener('keydown', (evt) => {
+    if (evt.key === 'Escape'){
+      closeAll();
+    }
+  });
+
+  // После отправки формы
+  form.addEventListener('submit', (evt) => {
+    const addListenersForBlock = function (block, button, inner) {
+      let flag = false;
+      button.addEventListener('click', () => {
+        block.remove();
+      });
+      window.addEventListener('keydown', (event) => {
+        if (event.key === 'Escape'){
+          block.remove();
+        }
+      });
+      window.addEventListener('mousedown', () => {
+        flag = true;
+      });
+      inner.addEventListener('mouseup', () => {
+        flag = false;
+      });
+      window.addEventListener('click', () => {
+        if (flag) {
+          block.remove();
+        }
+      });
+    };
+
+    const showSuccessMesage = function (templateSelector, buttonSelector, innerSelector) {
+      const template = document.querySelector(templateSelector).content.querySelector('section');
+      const success = template.cloneNode(true);
+      const successButton = success.querySelector(buttonSelector);
+      const successInner = success.querySelector(innerSelector);
+      addListenersForBlock(success, successButton, successInner);
+      body.append(success);
+    };
+    evt.preventDefault();
+    pristine.validate();
+    disableButton(submitButton);
+    submitButton.textContent = 'Опубликовываю...';
+    sendData(new FormData(evt.target))
+      .then(() => {
+        closeAll();
+        enableButton(submitButton);
+        showSuccessMesage('#success', '.success__button', '.success__inner');
+      })
+      .catch((err) => {
+        showSuccessMesage('#error', '.error__button', '.error__inner');
+        enableButton(submitButton);
+        submitButton.textContent = 'Опубликовывать';
+        throw new Error(err);
+      });
   });
 };
 
